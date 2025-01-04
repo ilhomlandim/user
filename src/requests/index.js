ff089fef10df2facd65a4ef1dfead13c911fe4c5
mirzoboxu <mirzoulugbekkhudoyberdiyev@gmail.com>
2025-01-04 19:36:46 +0500

    Update caching

--- src/requests/index.js
+++ src/requests/index.js
@@ -1,7 +1,11 @@
 import { baseURL } from "@/constants";
 
 export async function getData(route = "/materials") {
-  const req = await fetch(baseURL + route);
+  const req = await fetch(baseURL + route, {
+    headers: {
+      "Cache-Control": "no-store",
+    },
+  });
   if (req.status === 200) {
     const { data } = await req.json();
     return data;
