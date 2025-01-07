1879863f1caf8b84e5b4507e65a293e77ebb414a
oyatillo071 <oyatillotoshtemirov071@gmail.com>
2025-01-07 12:08:10 +0500

    chagne grid  in founders sections

--- src/components/Founders.jsx
+++ src/components/Founders.jsx
@@ -10,7 +10,7 @@ function Founders() {
         Sayt ijodkorlari
       </h2>
       {/* comment */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-2 gap-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 gap-6">
         {foundersInfo.map(
           ({ Fullname, position, work, logo, contacts }, index) => {
             return (
